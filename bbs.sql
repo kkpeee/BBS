c1244f85d866e957aa99061cb48dfdae963e3c5f
higashi <rin.y8b@gmail.com>
2016-05-23 16:06:22 +0900

    Add files via upload

--- bbs.sql
+++ bbs.sql
@@ -5,7 +5,7 @@ password varchar(255) not null,
 name varchar(10) not null,
 branch_id integer not null,
 department_id integer not null,
-account_op ,
+locked integer default 0
 );
 
 create table branches(
@@ -25,15 +25,15 @@ title varchar(50) not null,
 text text not null,
 category varchar(10) not null,
 user_id integer not null,
-text varchar(500) not null,
-insert_date timestamp not null,
-update_date timestamp not null
+created_at timestamp not null,
+updated_at timestamp not null
 );
 
 create table comments(
 id integer primary key not null auto_increment,
 text text not null,
 user_id integer not null,
-insert_date timestamp not null,
-update_date timestamp not null
+contribution_id integer not null,
+created_at timestamp not null,
+updated_at timestamp not null
 );
